0a6eebfc7a098e30ae573eae0ea412b73dfecd54
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2023-06-06 12:52:13 +0300

    Refactor app into modules
    Exercise 4.2

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+
+mongoose.connect(config.MONGO_URI)
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app

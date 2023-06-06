51939edc456083f06181b55a8b64f24910c02009
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2023-06-06 13:29:37 +0300

    Create module for logging

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
 mongoose.connect(config.MONGO_URI)
+    .then(() => logger.info('Connected to db'))
+    .catch((error) => logger.error('Error connecting to db', error.message))
 
 app.use(cors())
 app.use(express.json())

3f027994129e0c06679319cf58a9cdeb61eaccc2
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2023-06-09 14:28:00 +0300

    Config API testing and refactor
    Test that all blogs are returned as JSON. Exercise 4.8

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,70 +1,9 @@
 const listHelper = require('../utils/list_helper')
-
-const listWithOneBlog = [
-    {
-        _id: '5a422b891b54a676234d17fa',
-        title: 'First class tests',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-        likes: 10,
-        __v: 0
-    },
-]
-
-const listWithManyBlogs = [
-    {
-        _id: '5a422a851b54a676234d17f7',
-        title: 'React patterns',
-        author: 'Michael Chan',
-        url: 'https://reactpatterns.com/',
-        likes: 7,
-        __v: 0
-    },
-    {
-        _id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5,
-        __v: 0
-    },
-    {
-        _id: '5a422b3a1b54a676234d17f9',
-        title: 'Canonical string reduction',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-        likes: 12,
-        __v: 0
-    },
-    {
-        _id: '5a422b891b54a676234d17fa',
-        title: 'First class tests',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-        likes: 10,
-        __v: 0
-    },
-    {
-        _id: '5a422ba71b54a676234d17fb',
-        title: 'TDD harms architecture',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-        likes: 0,
-        __v: 0
-    },
-    {
-        _id: '5a422bc61b54a676234d17fc',
-        title: 'Type wars',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-        likes: 2,
-        __v: 0
-    }
-]
+const helper = require('./test_helper')
 
 describe('favoriteBlog', () => {
     test('returns a blog with the most likes', () => {
-        const favorite = listHelper.favoriteBlog(listWithManyBlogs)
+        const favorite = listHelper.favoriteBlog(helper.listWithManyBlogs)
         expect(favorite.title).toBe('Canonical string reduction')
         expect(favorite.author).toBe('Edsger W. Dijkstra')
         expect(favorite.likes).toBe(12)
@@ -77,10 +16,10 @@ describe('total likes', () => {
     })
 
     test('of list with one blog is calculated correctly', () => {
-        expect(listHelper.totalLikes(listWithOneBlog)).toBe(10)
+        expect(listHelper.totalLikes(helper.listWithOneBlog)).toBe(10)
     })
 
     test('of list with many blogs is calculated correctly', () => {
-        expect(listHelper.totalLikes(listWithManyBlogs)).toBe(36)
+        expect(listHelper.totalLikes(helper.listWithManyBlogs)).toBe(36)
     })
 })
--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 let PORT = process.env.PORT
-let MONGO_URI = process.env.MONGO_URI
+let MONGO_URI = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGO_URI
+    : process.env.MONGO_URI
 
 module.exports = {
     MONGO_URI,

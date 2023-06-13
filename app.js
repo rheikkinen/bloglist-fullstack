c512edd7e46a41c1aa4bb3686762838bd4886c69
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2023-06-13 15:35:45 +0300

    Config error handling

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+require('express-async-errors')
 const cors = require('cors')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
@@ -14,5 +16,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.errorHandler)
 
 module.exports = app

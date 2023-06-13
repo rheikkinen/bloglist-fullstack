6247e30a6db3cdf16c2ab504b64e2bc33cf1a6f8
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2023-06-13 18:29:31 +0300

    Add functionality for deleting a blog
    Write a test for the blog deletion. Exercise 4.13

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -10,62 +10,82 @@ beforeEach(async () => {
     await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+describe('when blogs are returned from the database', () => {
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+
+    test('all blogs are returned', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+    })
+
+    test('returned blogs have a property named id', async () => {
+        const response = await api.get('/api/blogs')
+        const blogs = response.body
+        blogs.forEach(blog => expect(blog.id).toBeDefined())
+    })
 })
 
-test('all blogs are returned', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(helper.initialBlogs.length)
+describe('adding a new blog', () => {
+    test('succeeds with valid details', async () => {
+        await api
+            .post('/api/blogs')
+            .send(helper.newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const allBlogs = await helper.blogsInDatabase()
+        expect(allBlogs).toHaveLength(helper.initialBlogs.length + 1)
+    })
+
+    test('added blog has zero likes by default', async () => {
+        const response = await api
+            .post('/api/blogs')
+            .send(helper.newBlog)
+
+        const addedBlog = response.body
+        expect(addedBlog.likes).toEqual(0)
+    })
+
+    test('fails if URL is not provided', async () => {
+        await api
+            .post('/api/blogs')
+            .send(helper.blogWithoutUrl)
+            .expect(400)
+
+        const allBlogs = await helper.blogsInDatabase()
+        expect(allBlogs).toHaveLength(helper.initialBlogs.length)
+    })
+
+    test('fails if title is not provided', async () => {
+        await api
+            .post('/api/blogs')
+            .send(helper.blogWithoutTitle)
+            .expect(400)
+
+        const allBlogs = await helper.blogsInDatabase()
+        expect(allBlogs).toHaveLength(helper.initialBlogs.length)
+    })
 })
 
-test('returned blogs have a property named id', async () => {
-    const response = await api.get('/api/blogs')
-    const blogs = response.body
-    blogs.forEach(blog => expect(blog.id).toBeDefined())
-})
+describe('deleting a blog', () => {
+    test('succeeds if the id is existing', async () => {
+        const blogsBeforeDeletion = await helper.blogsInDatabase()
+        const blogToBeDeleted = blogsBeforeDeletion[0]
+        expect(blogsBeforeDeletion).toContain(blogToBeDeleted)
 
-test('a valid blog can be added', async () => {
-    await api
-        .post('/api/blogs')
-        .send(helper.newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+        await api
+            .delete(`/api/blogs/${blogToBeDeleted.id}`)
+            .expect(204)
 
-    const allBlogs = await helper.blogsInDatabase()
-    expect(allBlogs).toHaveLength(helper.initialBlogs.length + 1)
-})
-
-test('a new blog has zero likes by default', async () => {
-    const response = await api
-        .post('/api/blogs')
-        .send(helper.newBlog)
-
-    const addedBlog = response.body
-    expect(addedBlog.likes).toEqual(0)
-})
-
-test('a blog without URL cannot be added', async () => {
-    await api
-        .post('/api/blogs')
-        .send(helper.blogWithoutUrl)
-        .expect(400)
-
-    const allBlogs = await helper.blogsInDatabase()
-    expect(allBlogs).toHaveLength(helper.initialBlogs.length)
-})
-
-test('a blog without title cannot be added', async () => {
-    await api
-        .post('/api/blogs')
-        .send(helper.blogWithoutTitle)
-        .expect(400)
-
-    const allBlogs = await helper.blogsInDatabase()
-    expect(allBlogs).toHaveLength(helper.initialBlogs.length)
+        const blogsAFterDeletion = await helper.blogsInDatabase()
+        expect(blogsAFterDeletion).toHaveLength(blogsBeforeDeletion.length - 1)
+        expect(blogsAFterDeletion).not.toContain(blogToBeDeleted)
+    })
 })
 
 afterAll(async () => await mongoose.connection.close())

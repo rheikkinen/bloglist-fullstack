62ce3dd8d76b6355dabff6631c2eaeb743acd67e
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2023-09-20 16:00:05 +0300

    Add e2e tests for the login functionality
    Also added a custom cypress command for registering a user. Exercise 5.18

--- frontend/cypress/e2e/blog_app.cy.js
+++ frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,7 @@
 describe('Blog app', () => {
   beforeEach(() => {
     cy.resetDatabase()
+    cy.registerUser({ username: 'testuser', password: 'testpassword' })
     cy.visit('/')
   })
 
@@ -8,5 +9,34 @@ describe('Blog app', () => {
     cy.contains('Log in to the Blog List App')
     cy.get('input[placeholder="Username"]')
     cy.get('input[placeholder="Password"]')
+  })
+
+  describe('Login', () => {
+    it('succeeds with correct credentials', () => {
+      cy.get('input[placeholder="Username"]').type('testuser')
+      cy.get('input[placeholder="Password"]').type('testpassword')
+      cy.get('button').contains('Log in').click()
+      cy.contains('Logged in as testuser')
+    })
+
+    it('fails with incorrect password', () => {
+      cy.get('input[placeholder="Username"]').type('testuser')
+      cy.get('input[placeholder="Password"]').type('wrong_password')
+      cy.get('button').contains('Log in').click()
+      cy.contains('Invalid username or password')
+      // The login form should still be visible
+      cy.get('input[placeholder="Username"]')
+      cy.get('input[placeholder="Password"]')
+    })
+
+    it('fails with incorrect username', () => {
+      cy.get('input[placeholder="Username"]').type('wrong_username')
+      cy.get('input[placeholder="Password"]').type('testpassword')
+      cy.get('button').contains('Log in').click()
+      cy.contains('Invalid username or password')
+      // The login form should still be visible
+      cy.get('input[placeholder="Username"]')
+      cy.get('input[placeholder="Password"]')
+    })
   })
 })
--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -1,3 +1,7 @@
 Cypress.Commands.add('resetDatabase', () => {
   cy.request('POST', `${Cypress.env('backendUrl')}/api/testing/reset`)
 })
+
+Cypress.Commands.add('registerUser', (user) => {
+  cy.request('POST', `${Cypress.env('backendUrl')}/api/users`, user)
+})

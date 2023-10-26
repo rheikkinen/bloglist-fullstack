d09466767dce4c296ce89a09f04b2d2284724849
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2023-10-26 18:24:17 +0300

    Add TokenExpiredError to errorHandler middleware

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -42,6 +42,8 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: error.message })
   } else if (error.name === 'JsonWebTokenError') {
     return response.status(400).json({ error: 'Token missing or invalid' })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(400).json({ error: 'Token expired' })
   } else if (error.name === 'CastError') {
     return response.status(400).json({ error: 'Id is incorrectly formatted' })
   }
